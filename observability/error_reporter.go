ce0d6aed40a237934603bb8e250019e5cd2f7c9d
Teodora Sandu <teodora.sandu@snyk.io>
2024-03-13 10:50:28 +0000

    refactor: remove tracker, logger, and error reporter

--- observability/error_reporter.go
+++
@@ -1,8 +0,0 @@
-package observability
-
-// ErrorReporter exposes functions using for reporting errors.
-type ErrorReporter interface {
-	FlushErrorReporting()
-	CaptureError(err error) bool
-	CaptureErrorAndReportAsIssue(path string, err error) bool
-}
