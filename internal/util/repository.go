4849a425d020a88928082d331a03b399e407ceb3
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2024-03-26 12:14:31 +0100

    chore: initial version of GetRepositoryUrl()

---
+++ internal/util/repository.go
@@ -0,0 +1,45 @@
+/*
+ * © 2024 Snyk Limited All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package util
+
+import (
+	"fmt"
+
+	"github.com/go-git/go-git/v5"
+)
+
+func GetRepositoryUrl(path string) (string, error) {
+	repo, err := git.PlainOpen(path)
+	if err != nil {
+		fmt.Errorf("open local repository: %w", err)
+		return "", err
+	}
+
+	remote, err := repo.Remote("origin")
+	if err != nil {
+		fmt.Errorf("get remote: %w", err)
+		return "", err
+	}
+
+	if len(remote.Config().URLs) == 0 {
+		return "", fmt.Errorf("no repository urls available")
+	}
+
+	repourl := remote.Config().URLs[0]
+
+	return repourl, nil
+}
---
+++ internal/util/repository_test.go
@@ -0,0 +1,31 @@
+/*
+ * © 2024 Snyk Limited All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package util
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_GetRepositoryUrl_no_repo(t *testing.T) {
+
+	actualUrl, err := GetRepositoryUrl("")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, actualUrl)
+	fmt.Println(actualUrl)
+}
