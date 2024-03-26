e8248cde51ef7a7628a149c876e31d45082e9548
Teodora Sandu <teodora.sandu@snyk.io>
2024-03-26 10:58:47 +0000

    test: add an example test

--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/puzpuzpuz/xsync v1.5.2
 	github.com/rs/zerolog v1.32.0
-	github.com/stretchr/testify v1.8.4
+	github.com/stretchr/testify v1.9.0
 	golang.org/x/net v0.22.0
 )
 
@@ -63,6 +63,7 @@ require (
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/sergi/go-diff v1.3.1 // indirect
 	github.com/skeema/knownhosts v1.2.1 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
---
+++ internal/workspace/2024-03-12/client_test.go
@@ -0,0 +1,72 @@
+package v20240312_test
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+
+	v20240312 "github.com/snyk/code-client-go/internal/workspace/2024-03-12"
+	externalRef3 "github.com/snyk/code-client-go/internal/workspace/2024-03-12/workspaces"
+)
+
+// HTTPRequestDoerMock mocks the interface HttpRequestDoerMock.
+type HTTPRequestDoerMock struct {
+	mock.Mock
+}
+
+func (m *HTTPRequestDoerMock) Do(req *http.Request) (*http.Response, error) {
+	args := m.Called(req)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*http.Response), args.Error(1)
+}
+
+func TestWorkspace_CreateWorkspaceWithBody(t *testing.T) {
+	doer := &HTTPRequestDoerMock{}
+	doer.On("Do", mock.Anything).Return(nil, nil).Run(func(args mock.Arguments) {
+		req, ok := args.Get(0).(*http.Request)
+		assert.True(t, ok)
+		assert.Equal(t, "/https//api.snyk.io/rest/orgs/e7ea34c9-de0f-422c-bf2c-4654c2e2da90/workspaces?version=2024-03-12~experimental", req.URL.String())
+	})
+	client, err := v20240312.NewClientWithResponses("https//api.snyk.io/rest", v20240312.WithHTTPClient(doer))
+	require.NoError(t, err)
+
+	orgUUID := uuid.MustParse("e7ea34c9-de0f-422c-bf2c-4654c2e2da90")
+	requestId := uuid.New()
+	_, err = client.CreateWorkspaceWithApplicationVndAPIPlusJSONBody(context.Background(), orgUUID, &v20240312.CreateWorkspaceParams{
+		Version:       "2024-03-12~experimental",
+		SnykRequestId: requestId,
+	}, v20240312.CreateWorkspaceApplicationVndAPIPlusJSONRequestBody{
+		Data: struct {
+			Attributes struct {
+				BundleId      string                                                       `json:"bundle_id"`
+				RepositoryUri string                                                       `json:"repository_uri"`
+				WorkspaceType externalRef3.WorkspacePostRequestDataAttributesWorkspaceType `json:"workspace_type"`
+			} `json:"attributes"`
+			Type externalRef3.WorkspacePostRequestDataType `json:"type"`
+		}(struct {
+			Attributes struct {
+				BundleId      string                                                       `json:"bundle_id"`
+				RepositoryUri string                                                       `json:"repository_uri"`
+				WorkspaceType externalRef3.WorkspacePostRequestDataAttributesWorkspaceType `json:"workspace_type"`
+			}
+			Type externalRef3.WorkspacePostRequestDataType
+		}{Attributes: struct {
+			BundleId      string                                                       `json:"bundle_id"`
+			RepositoryUri string                                                       `json:"repository_uri"`
+			WorkspaceType externalRef3.WorkspacePostRequestDataAttributesWorkspaceType `json:"workspace_type"`
+		}(struct {
+			BundleId      string
+			RepositoryUri string
+			WorkspaceType externalRef3.WorkspacePostRequestDataAttributesWorkspaceType
+		}{BundleId: "bundleId", RepositoryUri: "repositoryUri", WorkspaceType: "workspaceUri"}), Type: "workspace"}),
+	})
+	require.NoError(t, err)
+	doer.AssertExpectations(t)
+}
