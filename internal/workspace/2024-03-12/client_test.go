bd1ed9340c988a03b285b8600de770393141a5b4
Teodora Sandu <teodora.sandu@snyk.io>
2024-04-02 09:51:09 +0100

    test: add failure test

--- internal/workspace/2024-03-12/client_test.go
+++ internal/workspace/2024-03-12/client_test.go
@@ -2,6 +2,7 @@ package v20240312_test
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"testing"
 
@@ -27,7 +28,7 @@ func (m *HTTPRequestDoerMock) Do(req *http.Request) (*http.Response, error) {
 	return args.Get(0).(*http.Response), args.Error(1)
 }
 
-func TestWorkspace_CreateWorkspaceWithBody(t *testing.T) {
+func TestWorkspace_CreateWorkspaceWithApplicationVndAPIPlusJSONBody_Success(t *testing.T) {
 	doer := &HTTPRequestDoerMock{}
 	doer.On("Do", mock.Anything).Return(nil, nil).Run(func(args mock.Arguments) {
 		req, ok := args.Get(0).(*http.Request)
@@ -68,5 +69,49 @@ func TestWorkspace_CreateWorkspaceWithBody(t *testing.T) {
 		}{BundleId: "bundleId", RepositoryUri: "repositoryUri", WorkspaceType: "workspaceUri"}), Type: "workspace"}),
 	})
 	require.NoError(t, err)
+	doer.AssertExpectations(t)
+}
+
+func TestWorkspace_CreateWorkspaceWithApplicationVndAPIPlusJSONBody_Failure(t *testing.T) {
+	doer := &HTTPRequestDoerMock{}
+	doer.On("Do", mock.Anything).Return(nil, errors.New("something went wrong")).Run(func(args mock.Arguments) {
+		req, ok := args.Get(0).(*http.Request)
+		assert.True(t, ok)
+		assert.Equal(t, "/https//api.snyk.io/rest/orgs/e7ea34c9-de0f-422c-bf2c-4654c2e2da90/workspaces?version=2024-03-12~experimental", req.URL.String())
+	})
+	client, err := v20240312.NewClientWithResponses("https//api.snyk.io/rest", v20240312.WithHTTPClient(doer))
+	require.NoError(t, err)
+
+	orgUUID := uuid.MustParse("e7ea34c9-de0f-422c-bf2c-4654c2e2da90")
+	requestId := uuid.New()
+	_, err = client.CreateWorkspaceWithApplicationVndAPIPlusJSONBody(context.Background(), orgUUID, &v20240312.CreateWorkspaceParams{
+		Version:       "2024-03-12~experimental",
+		SnykRequestId: requestId,
+	}, v20240312.CreateWorkspaceApplicationVndAPIPlusJSONRequestBody{
+		Data: struct {
+			Attributes struct {
+				BundleId      string                                                       `json:"bundle_id"`
+				RepositoryUri string                                                       `json:"repository_uri"`
+				WorkspaceType externalRef3.WorkspacePostRequestDataAttributesWorkspaceType `json:"workspace_type"`
+			} `json:"attributes"`
+			Type externalRef3.WorkspacePostRequestDataType `json:"type"`
+		}(struct {
+			Attributes struct {
+				BundleId      string                                                       `json:"bundle_id"`
+				RepositoryUri string                                                       `json:"repository_uri"`
+				WorkspaceType externalRef3.WorkspacePostRequestDataAttributesWorkspaceType `json:"workspace_type"`
+			}
+			Type externalRef3.WorkspacePostRequestDataType
+		}{Attributes: struct {
+			BundleId      string                                                       `json:"bundle_id"`
+			RepositoryUri string                                                       `json:"repository_uri"`
+			WorkspaceType externalRef3.WorkspacePostRequestDataAttributesWorkspaceType `json:"workspace_type"`
+		}(struct {
+			BundleId      string
+			RepositoryUri string
+			WorkspaceType externalRef3.WorkspacePostRequestDataAttributesWorkspaceType
+		}{BundleId: "bundleId", RepositoryUri: "repositoryUri", WorkspaceType: "workspaceUri"}), Type: "workspace"}),
+	})
+	require.Error(t, err)
 	doer.AssertExpectations(t)
 }
