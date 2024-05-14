b3a36fe43981e4c1bff3cba857602419fe55be15
Teodora Sandu <teodora.sandu@snyk.io>
2024-05-14 17:23:32 +0100

    test: fix git clone tests

--- scan_smoke_test.go
+++ scan_smoke_test.go
@@ -22,12 +22,10 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"testing"
 	"time"
 
-	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/google/uuid"
@@ -44,7 +42,7 @@ func Test_SmokeScan_HTTPS(t *testing.T) {
 	if os.Getenv("SMOKE_TESTS") != "true" {
 		t.Skip()
 	}
-	var cloneTargetDir, err = setupCustomTestRepo(t, "https://github.com/snyk-labs/nodejs-goof", "0336589")
+	var cloneTargetDir, err = testutil.SetupCustomTestRepo(t, "https://github.com/snyk-labs/nodejs-goof", "0336589", "", "")
 	assert.NoError(t, err)
 
 	target, err := scan.NewRepositoryTarget(cloneTargetDir)
@@ -90,7 +88,7 @@ func Test_SmokeScan_SSH(t *testing.T) {
 	if os.Getenv("SMOKE_TESTS") != "true" {
 		t.Skip()
 	}
-	var cloneTargetDir, err = setupCustomTestRepo(t, "git@github.com:snyk-labs/nodejs-goof", "0336589")
+	var cloneTargetDir, err = testutil.SetupCustomTestRepo(t, "git@github.com:snyk-labs/nodejs-goof", "0336589", "", "")
 	assert.NoError(t, err)
 
 	target, err := scan.NewRepositoryTarget(cloneTargetDir)
@@ -170,36 +168,6 @@ func Test_SmokeScan_SubFolder(t *testing.T) {
 	require.NoError(t, scanErr)
 	require.NotEmpty(t, bundleHash)
 	require.NotNil(t, response)
-}
-
-func setupCustomTestRepo(t *testing.T, url string, targetCommit string) (string, error) {
-	t.Helper()
-	tempDir := t.TempDir()
-	repoDir := "1"
-	absoluteCloneRepoDir := filepath.Join(tempDir, repoDir)
-	cmd := []string{"clone", url, repoDir}
-	log.Debug().Interface("cmd", cmd).Msg("clone command")
-	clone := exec.Command("git", cmd...)
-	clone.Dir = tempDir
-	reset := exec.Command("git", "reset", "--hard", targetCommit)
-	reset.Dir = absoluteCloneRepoDir
-
-	clean := exec.Command("git", "clean", "--force")
-	clean.Dir = absoluteCloneRepoDir
-
-	output, err := clone.CombinedOutput()
-	if err != nil {
-		t.Fatal(err, "clone didn't work")
-	}
-
-	log.Debug().Msg(string(output))
-	output, _ = reset.CombinedOutput()
-
-	log.Debug().Msg(string(output))
-	output, err = clean.CombinedOutput()
-
-	log.Debug().Msg(string(output))
-	return absoluteCloneRepoDir, err
 }
 
 type TestAuthRoundTripper struct {
