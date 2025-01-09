8bdbca1b03b574fc760dfe02dc44f9114918c669
Peter Schäfer <101886095+PeterSchafer@users.noreply.github.com>
2025-01-09 12:12:07 +0100

    chore: update codegen to v2

--- tools.go
+++ tools.go
@@ -3,7 +3,7 @@
 package codeClient
 
 import (
-	_ "github.com/deepmap/oapi-codegen/cmd/oapi-codegen"
 	_ "github.com/golang/mock/mockgen"
+	_ "github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen"
 	_ "github.com/pact-foundation/pact-go/v2"
 )
