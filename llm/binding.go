5228992ceb7fdaa93cc72b5a6aa022054e3e8b64
Bastian Doetsch <bastian.doetsch@snyk.io>
2025-02-11 14:05:04 +0100

    refactor: move options to their own file
    The provided diff shows that the code for the `Option` functions (like `WithHTTPClient`, `WithEndpoint`, etc.) and the related type definition have been moved from `llm/binding.go` to a new file `llm/options.go`.  This is a good refactoring as it separates the concerns of options management from the core LLM binding logic.  The change itself is straightforward and doesn't introduce any logical modifications, only structural ones.

--- llm/binding.go
+++ llm/binding.go
@@ -51,47 +51,6 @@ func (d *DeepcodeLLMBinding) Explain(input string, format OutputFormat, output c
 	panic("implement me")
 }
 
-type Option func(*DeepcodeLLMBinding)
-
-func WithHTTPClient(httpClientFunc func() *http.Client) func(*DeepcodeLLMBinding) {
-	return func(binding *DeepcodeLLMBinding) {
-		binding.httpClientFunc = httpClientFunc
-	}
-}
-
-func WithEndpoint(endpoint *url.URL) func(*DeepcodeLLMBinding) {
-	return func(binding *DeepcodeLLMBinding) {
-		binding.endpoint = endpoint
-	}
-}
-
-func WithLogger(logger zerolog.Logger) func(*DeepcodeLLMBinding) {
-	return func(binding *DeepcodeLLMBinding) {
-		binding.logger = logger
-	}
-}
-
-func WithOutputChannel(outputChannel chan<- string) func(*DeepcodeLLMBinding) {
-	return func(binding *DeepcodeLLMBinding) {
-		binding.outputChannel = outputChannel
-	}
-}
-
-func WithOutputFormat(outputFormat OutputFormat) func(*DeepcodeLLMBinding) {
-	return func(binding *DeepcodeLLMBinding) {
-		if outputFormat != HTML && outputFormat != JSON && outputFormat != MarkDown {
-			return
-		}
-		binding.outputFormat = outputFormat
-	}
-}
-
-func WithInstrumentor(instrumentor observability.Instrumentor) func(*DeepcodeLLMBinding) {
-	return func(binding *DeepcodeLLMBinding) {
-		binding.instrumentor = instrumentor
-	}
-}
-
 func NewDeepcodeLLMBinding(opts ...Option) *DeepcodeLLMBinding {
 	endpoint, err := url.Parse(defaultEndpointURL)
 	if err != nil {
