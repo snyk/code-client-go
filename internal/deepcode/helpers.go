30592824b40dc41d7a720b2f27b2581fdc3e7808
Andrew Robinson Hodges <andrew.robinsonhodges@snyk.io>
2025-04-07 10:08:43 +0100

    feat: improve bundle performance [IDE-937] (#87)

--- internal/deepcode/helpers.go
+++ internal/deepcode/helpers.go
@@ -20,14 +20,16 @@ import (
 )
 
 type BundleFile struct {
-	Hash    string `json:"hash"`
-	Content string `json:"content"`
+	Hash        string `json:"hash"`
+	Content     string `json:"content"`
+	ContentSize int    `json:"size"`
 }
 
 func BundleFileFrom(content []byte) BundleFile {
 	file := BundleFile{
-		Hash:    util.Hash(content),
-		Content: string(content),
+		Hash:        util.Hash(content),
+		Content:     "", // We create the bundleFile empty, and enrich  with content later.
+		ContentSize: len(content),
 	}
 	return file
 }
--- internal/util/hash.go
+++ internal/util/hash.go
@@ -26,13 +26,18 @@ import (
 )
 
 func Hash(content []byte) string {
-	byteReader := bytes.NewReader(content)
-	reader, _ := charset.NewReaderLabel("UTF-8", byteReader)
-	utf8content, err := io.ReadAll(reader)
+	utf8content, err := ConvertToUTF8(content)
 	if err != nil {
 		utf8content = content
 	}
 	b := sha256.Sum256(utf8content)
 	sum256 := hex.EncodeToString(b[:])
 	return sum256
+}
+
+func ConvertToUTF8(content []byte) ([]byte, error) {
+	byteReader := bytes.NewReader(content)
+	reader, _ := charset.NewReaderLabel("UTF-8", byteReader)
+	utf8content, err := io.ReadAll(reader)
+	return utf8content, err
 }
