c1fb6e504e65d79ddc214fcac9178c031e5f5354
Thomas Schafer <thomasschafer97@gmail.com>
2025-04-16 10:58:51 +0100

    chore: expose Bundle interface

---
+++ bundle/bundle_types.go
@@ -0,0 +1,12 @@
+package bundle
+
+import (
+	"github.com/snyk/code-client-go/internal/bundle"
+	"github.com/snyk/code-client-go/internal/deepcode"
+)
+
+type Batch = bundle.Batch
+
+type Bundle = bundle.Bundle
+
+type BundleFile = deepcode.BundleFile
