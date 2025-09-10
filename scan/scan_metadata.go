69c811e014827c32b9aad06c6f3f682b3b536eb1
Andrew Robinson Hodges <andrew.robinsonhodges@snyk.io>
2025-09-10 17:30:07 +0100

    refactor: move scan contect code into code-client-go for reuse

--- scan/scan_metadata.go
+++ scan/scan_metadata.go
@@ -1,5 +1,7 @@
 package scan
 
+import "context"
+
 type contextKey string
 
 const InitiatorKey = contextKey("initiator")
@@ -7,4 +9,29 @@ const InitiatorKey = contextKey("initiator")
 type ResultMetaData struct {
 	FindingsUrl string
 	WebUiUrl    string
+}
+
+type ScanSource string
+
+func (s ScanSource) String() string {
+	return string(s)
+}
+
+const (
+	LLM ScanSource = "LLM"
+	IDE ScanSource = "IDE"
+	CLI ScanSource = "CLI"
+)
+
+type scanSourceKeyType int
+
+var scanSourceKey scanSourceKeyType
+
+func NewContextWithScanSource(ctx context.Context, source ScanSource) context.Context {
+	return context.WithValue(ctx, scanSourceKey, source)
+}
+
+func ScanSourceFromContext(ctx context.Context) (ScanSource, bool) {
+	s, ok := ctx.Value(scanSourceKey).(ScanSource)
+	return s, ok
 }
